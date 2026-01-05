d455c9b3b480cbbf6160a58795193546366d5f9a
Diogo dos Santos <kikumbanationsa@gmail.com>
2026-01-05 22:33:25 +0100

    Commit That
    ...

---
+++ calculo_de_velocidade.py
@@ -0,0 +1,15 @@
+nome= str(input('Insira por favor o seu nome '))
+
+print('Nome do motoritsa {} ' .format(nome))
+
+
+velocidade= float(input('A velocidade atingida km/h...'))
+
+print('Velocidade atindiga  {} km/h' .format(velocidade))
+
+
+if velocidade<=100:print('No limite da velocidade...')
+
+else:print('Limite de velcidade excedido, vai ser multado...')
+
+
---
+++ equação do 2°grau.py
@@ -0,0 +1,28 @@
+post = "equação do 2° Grau"
+
+
+print(post)
+
+a = int(input('valor de a'))
+
+print('valor de a {} ' .format(a))
+
+x = int(input('Valor de x'))
+
+print('valor de x ' .format(x))
+
+b = int(input('valor de b'))
+
+print('valor de b  {}' .format(b))
+
+c = int(input('valor de  c  '))
+
+print('valor de c  {}' .format(c))
+
+print('Equação ', a*x**2+c==0 )
+
+
+
+
+
+
